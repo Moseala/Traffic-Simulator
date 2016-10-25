165d5abeb94874f875b64df741fd883f85e906c3
ec007 <ec007@1TL207-04.tamuct.edu>
2016-10-25 15:37:42 -0500

    Refers to #16 Added feedRoad attribute and .equals/.getIdentifier methods. Updated javadoc to reflect multiple iterations (1.0 -> 1.00)

--- TrafficSimulator/src/main/java/com/mycompany/trafficsimulator/TrafficSignal.java
+++ TrafficSimulator/src/main/java/com/mycompany/trafficsimulator/TrafficSignal.java
@@ -14,24 +14,33 @@ import java.util.Queue;
  * <p> <b>Date Created: </b>October 24, 2016 
  * <p> <b>Version Comments:</b> 
  *      <ul> 
- *          <li> 1.0a | 10/24/2016: Initial commit </li> 
+ *          <li> 1.00a | 10/24/2016: Initial commit </li> 
+ *          <li> 1.01a | 10/25/2016: Added source road that feeds this traffic 
+ *                                  signal for functionality with signal groups 
+ *                                  and map. Also added .equals comparator and getIdentifier</li> 
  *      </ul>
  */
 public class TrafficSignal {
     final int signalType;
     private Queue<Car> carQueue;
+    private Road sourceRoad;
+    private String identifier;
     
     /**
      * Constructor for this class, accepts an integer as signal type,
      * MUST USE SignalBehavior.TYPE_#, in order to function properly
      * 
-     * @param SIGNAL_TYPE    value of the signal type, must come from SignalBehavior class'
+     * @param SIGNAL_TYPE   value of the signal type, must come from SignalBehavior class'
      *                      dictionary 
+     * @param feedingRoad   the road that feeds into this traffic signal
+     * @param uniqueIdentifier  the unique identifier that represents this traffic signal.
      * @author Erik Clary
-     * @since 1.0a
+     * @since 1.00a
      */
-    public TrafficSignal(int SIGNAL_TYPE){
+    public TrafficSignal(int SIGNAL_TYPE, Road feedingRoad, String uniqueIdentifier){
         this.signalType = SIGNAL_TYPE;
+        sourceRoad = feedingRoad;
+        identifier = uniqueIdentifier;
     }
     
     /**
@@ -39,7 +48,7 @@ public class TrafficSignal {
      * 
      * @param inCar         The car to be added to this queue.
      * @author Erik Clary
-     * @since 1.0a
+     * @since 1.00a
      */
     public void addCar(Car inCar){
         if(!carQueue.add(inCar))
@@ -51,7 +60,7 @@ public class TrafficSignal {
      * 
      * @return a Car object
      * @author Erik Clary
-     * @since 1.0a
+     * @since 1.00a
      */
     public Car removeNextCar(){
         return carQueue.poll();
@@ -62,9 +71,33 @@ public class TrafficSignal {
      * 
      * @return a SignalBehavior object representative of this signal's behavior
      * @author Erik Clary
-     * @since 1.0a
+     * @since 1.00a
      */
     public SignalBehavior getBehavior(){
         return new SignalBehavior(signalType); //OPTIMIZATION: move this creation to constructor, then return the contained object if runtimes are large, and memory is not
     }
+    
+    /**
+     * getIdentifier method returns the unique identifier attached to this TrafficSignal
+     * 
+     * @return String: the identifier unique to this TrafficSignal
+     * @author Erik Clary
+     * @since 1.01a
+     */
+    public String getIdentifier(){
+        return identifier;
+    }
+    
+    /**
+     * equals compares two traffic signals, true if equal, false if they are not
+     * this method compares the unique identifiers attached to the signal.
+     * 
+     * @param TrafficSignal     The traffic signal to compare this to
+     * @author Erik Clary
+     * @since 1.01a
+     */
+    public boolean equals(TrafficSignal E){
+        return this.identifier.equals(E.getIdentifier());
+    }
+    
 }
